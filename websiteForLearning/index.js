4ae259e9c25c9d71126d52e5d69440f7b8083623
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2024-05-20 13:00:27 +0200

    Added api with weather

--- websiteForLearning/index.js
+++ websiteForLearning/index.js
@@ -8,57 +8,6 @@ document.getElementById('colorSwitch').addEventListener('change', function() {
     }
 });
 
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
-const countLabel = document.getElementById("countLabel");
-
-let count = 0;
-
-increase.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
-
-decrease.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
-
-reset.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
-
-const myCheckbox = document.getElementById("myCheckbox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCardBtn = document.getElementById("masterCardBtn");
-const blikBtn = document.getElementById("blikBtn");
-const mySubmit = document.getElementById("mySubmit");
-const checkboxResult = document.getElementById("checkboxResult");
-const paymentResult = document.getElementById("paymentResult");
-
-mySubmit.onclick = function(){
-    if(myCheckbox.checked){
-        checkboxResult.textContent = "zaznaczone";
-    }
-    else{
-        checkboxResult.textContent = "nie zaznaczone";
-    }
-
-    if(visaBtn.checked){
-        paymentResult.textContent="Wybrano visa";
-    }
-    else  if(masterCardBtn.checked){
-        paymentResult.textContent="Wybrano masterCard";
-    }
-    else  if(blikBtn.checked){
-        paymentResult.textContent="Wybrano blik";
-    }
-    else{
-        paymentResult.textContent="pick smth";
-    }
-}
 
 
 const temperature = document.getElementById("temperature");
@@ -296,4 +245,101 @@ function playGame(playerChoice) {
             break;
 
     }
+}
+
+//weather
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "e35f3a9a2492dfe7eeeddc3fa10b52df";
+
+weatherForm.addEventListener("submit", async event => {
+    event.preventDefault();
+    const city = cityInput.value;
+    if(city){
+        try{
+            const weatherData = await getWeatherDate(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a city");
+    }
+});
+
+async function getWeatherDate(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
+    return await response.json();
+}
+
+function displayWeatherInfo(data){
+    const { name: city, main: { temp, humidity }, weather: [{ description, id }] } = data;
+    
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherId){
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "🌩️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌨️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "❄️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "🌈";
+    }
+}
+
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 }
